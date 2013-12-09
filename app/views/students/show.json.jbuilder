915f7a61cc632d34d9758a1d9a5f490cf0974dd3
Ryan Santiago <santiagoryanf@gmail.com>
2013-12-09 12:45:34 +0800

    twitter-bootstrap

---
+++ app/views/students/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @student, :id, :name, :age, :address, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webapp::Application.config.secret_key_base = '3da16761866fd22ff613499892e9af5d80e2b05b4237e0acd85f4a57270175dc66b6efe98526ec07289832c7df0ed6b64f61d920b16e489fd601736c6113d4ae'
